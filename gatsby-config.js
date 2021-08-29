3184676ada864b044eb7f83448c98943ff195718
Paul Mucur <mudge@mudge.name>
2021-08-29 14:18:01 +0100

    Commit of minimal Prismic site with passing tests

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-prismic-previews",
+      options: {
+        repositoryName: "rpf-products",
+      },
+    },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: "rpf-products",
+        schemas: {
+          case_study: require("./custom_types/case_study.json"),
+          category: require("./custom_types/category.json"),
+          product: require("./custom_types/product.json"),
+        },
+      },
+    },
+  ],
 };
